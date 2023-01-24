caf5ffd7cdca4ba06bacc9eb685ec0d24f40a8e1
khans8890 <kansumaira@gmail.com>
2023-01-24 18:22:38 -0500

    feat: completed all of exercises.test.js

--- exercises.js
+++ exercises.js
@@ -1,22 +1,42 @@
-function octalToDecimal() {
-  //Write code here
-}
+function octalToDecimal(str) {
+  let arr = str.split('').reverse();
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let numConv = Number(arr[i]);
+    let expNum = 8 ** i;
+    sum += expNum * numConv;
+  }
+  return sum;
+};
 
-function anagram() {
-  //Write code here
-}
+function anagram(word, arr) {
+  let newArr = [];
+  let sorting = word.split('').sort().join();
+  arr.filter(a => a.split('').sort().join() === sorting ? newArr.push(a) : "Try again.");
+  return newArr;
+};
 
-function triangle() {
-  //Write code here
-}
+function triangle(a, b, c) {
+  return a <= 0 || b <= 0 || c <= 0 || a + b + c < 180 ? "invalid"
+  : a == 90 || b == 90 || c == 90 ? "right"
+  : a > 90 || b > 90 || c > 90 ? "obtuse"
+  : a < 90 || b < 90 || c < 90 ? "acute" : "not a triangle";
+};
 
-function fridayThe13ths() {
-  //Write code here
-}
+function fridayThe13ths(year) {
+  let count = 0;
+  for (let i = 0; i <= 12; i++) {
+    let date = new Date(year, i, 13);
+    if (date.getDay() === 5) {
+      count += 1;
+    }
+  }
+  return count;
+};
 
 module.exports = {
   octalToDecimal,
   anagram,
   triangle,
-  fridayThe13ths,
+  fridayThe13ths
 };
--- exercises.test.js
+++ exercises.test.js
@@ -8,3 +8,33 @@ test('Question 0: add', () => {
   expect(exercises.add(1, -1)).toEqual(0);
 });
 */
+
+test('Question 1: octalToDecimal', () => {
+  expect(exercises.octalToDecimal('1')).toEqual(1)
+  expect(exercises.octalToDecimal('10')).toEqual(8)
+  expect(exercises.octalToDecimal('130')).toEqual(88)
+  expect(exercises.octalToDecimal('17')).toEqual(15)
+  expect(exercises.octalToDecimal('2047')).toEqual(1063)
+  expect(exercises.octalToDecimal('11')).toEqual(9)
+})
+
+test('Question 2: anagram', () => {
+  expect(exercises.anagram('listen', ['enlists', 'google', 'inlets', 'banana'])).toEqual([ "inlets" ])
+  expect(exercises.anagram('listen', ['enlist', 'google', 'inlets', 'banana'])).toEqual([ "enlist", "inlets" ])
+})
+
+test('Question 1: ', () => {
+  expect(exercises.triangle(60, 70, 50)).toEqual("acute")
+  expect(exercises.triangle(30, 90, 60)).toEqual("right")
+  expect(exercises.triangle(120, 50, 10)).toEqual("obtuse")
+  expect(exercises.triangle(0, 90, 90)).toEqual("invalid")
+  expect(exercises.triangle(50, 50, 50)).toEqual("invalid")
+
+})
+
+
+test('Question 1: ', () => {
+  expect(exercises.fridayThe13ths(1986)).toEqual(1)
+  expect(exercises.fridayThe13ths(2015)).toEqual(3)
+  expect(exercises.fridayThe13ths(2017)).toEqual(2)
+})
